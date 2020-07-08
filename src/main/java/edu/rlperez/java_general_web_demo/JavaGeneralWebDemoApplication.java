3e0f4ee9ed523e490f153e767df9defb66352dc2
Rigoberto L. Perez <rlperez@udel.edu>
2020-07-08 16:24:38 -0400

    Init commit

---
+++ src/main/java/edu/rlperez/java_general_web_demo/JavaGeneralWebDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.rlperez.java_general_web_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGeneralWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGeneralWebDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/rlperez/java_general_web_demo/JavaGeneralWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rlperez.java_general_web_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGeneralWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
