6bfccca028987730b6b9fa31eafc2ed1d163f518
Rigoberto L. Perez <rlperez@udel.edu>
2020-07-14 00:14:01 -0400

    Add getter by external id

--- src/main/java/edu/rlperez/java_general_web_demo/repositories/BreweryRepository.java
+++ src/main/java/edu/rlperez/java_general_web_demo/repositories/BreweryRepository.java
@@ -4,6 +4,9 @@ import edu.rlperez.java_general_web_demo.models.Brewery;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BreweryRepository extends CrudRepository<Brewery, Long> {
+    Optional<Brewery> findByExternalId(long id);
 }
