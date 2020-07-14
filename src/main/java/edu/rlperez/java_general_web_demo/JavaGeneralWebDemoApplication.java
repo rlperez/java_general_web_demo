b8a03d60261de92cf789c61a7e545a520634bc3c
Rigoberto L. Perez <rlperez@udel.edu>
2020-07-14 00:12:40 -0400

    Enable scheduling

--- src/main/java/edu/rlperez/java_general_web_demo/JavaGeneralWebDemoApplication.java
+++ src/main/java/edu/rlperez/java_general_web_demo/JavaGeneralWebDemoApplication.java
@@ -2,8 +2,12 @@ package edu.rlperez.java_general_web_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class JavaGeneralWebDemoApplication {
 
 	public static void main(String[] args) {
