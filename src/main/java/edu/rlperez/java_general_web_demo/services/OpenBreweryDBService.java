840f1f50db79fe34541f8661713cee63327c8d7a
Rigoberto L. Perez <rlperez@udel.edu>
2020-07-14 00:14:24 -0400

    First draft of data fetching logic. Needs more work

--- src/main/java/edu/rlperez/java_general_web_demo/services/OpenBreweryDBService.java
+++ src/main/java/edu/rlperez/java_general_web_demo/services/OpenBreweryDBService.java
@@ -1,6 +1,8 @@
 package edu.rlperez.java_general_web_demo.services;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+import edu.rlperez.java_general_web_demo.models.Brewery;
 import edu.rlperez.java_general_web_demo.models.BreweryType;
 import edu.rlperez.java_general_web_demo.repositories.BreweryRepository;
 import edu.rlperez.java_general_web_demo.repositories.BreweryTypeRepository;
@@ -10,12 +12,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static java.net.http.HttpClient.*;
+
 @Service
 public class OpenBreweryDBService {
 
     private final BreweryRepository breweryRepository;
 
     private final BreweryTypeRepository breweryTypeRepository;
+
+    private final String OPENBREWERYDB_URL = "https://api.openbrewerydb.org/breweries?page=%d&per_page=50";
+
+    private final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+            .optionalStart()
+            .appendOffset("+HHMM", "+0000").optionalEnd()
+            .optionalStart()
+            .appendZoneId()
+            .parseDefaulting(ChronoField.OFFSET_SECONDS, 0)
+            .optionalEnd()
+            .toFormatter()
+            .withZone(ZoneOffset.UTC);
 
     @Autowired
     public OpenBreweryDBService(BreweryRepository breweryRepository, BreweryTypeRepository breweryTypeRepository) {
@@ -24,11 +55,96 @@ public class OpenBreweryDBService {
     }
 
     // Run every day from the time the service starts.
-    @Scheduled(initialDelay = 0, fixedDelay = 86400000)
-    public void perform() throws Exception {
+    @Scheduled(initialDelay = 0, fixedRate = 86400000)
+    public void perform() {
+        HttpClient client = getClient();
+        int exceptionCount = 0;
+        var requests = new LinkedList<CompletableFuture<Boolean>>();
+
+        for (int i = 1; ; i++) {
+            HttpRequest request = getRequest(i);
+
+            requests.add(client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(this::handleBody));
+
+            if (requests.size() > 10) {
+                try {
+                    if (reachedLastPage(requests)) break;
+                    else requests.clear();
+                } catch (InterruptedException | ExecutionException e) {
+                    exceptionCount++;
+                    e.printStackTrace();
+                }
+            } else if (exceptionCount > 10) {
+                System.err.println("OpenBreweryDB exception count exceeded.");
+                break;
+            }
+        }
+    }
+
+    private boolean handleBody(String requestBody) {
+        BreweryResponse[] breweryResponses = new Gson().fromJson(requestBody, BreweryResponse[].class);
+        return Arrays.stream(breweryResponses).map(breweryResponse -> {
+            Brewery brewery = breweryRepository.findByExternalId(breweryResponse.getId()).orElse(new Brewery());
+            LocalDateTime responseUpdatedAt = LocalDateTime.parse(breweryResponse.getUpdatedAt(), DATE_TIME_FORMATTER);
+            if (brewery.getUpdatedAt().toEpochSecond(ZoneOffset.UTC) < responseUpdatedAt.toEpochSecond(ZoneOffset.UTC)) {
+                // Source data updated so use that. A brewery likely updated information.
+                brewery.setExternalId(breweryResponse.getId());
+                brewery.setName(breweryResponse.getName());
+                brewery.setStreet(breweryResponse.getStreet());
+                brewery.setCity(breweryResponse.getCity());
+                brewery.setState(breweryResponse.getState());
+                brewery.setPostalCode(breweryResponse.getPostalCode());
+                brewery.setCountry(breweryResponse.getCountry());
+                brewery.setPhone(breweryResponse.getPhone());
+                brewery.setWebsiteUrl(breweryResponse.getWebsiteUrl());
+                brewery.setUpdatedAt(LocalDateTime.now());
+
+                Optional<BreweryType> maybeType = breweryTypeRepository
+                        .findByType(breweryResponse.getBreweryType().toLowerCase());
+
+                if (maybeType.isEmpty()) {
+                    BreweryType breweryType = new BreweryType();
+                    breweryType.setType(breweryResponse.getBreweryType().toLowerCase());
+                    brewery.setBreweryType(breweryTypeRepository.save(breweryType));
+                } else {
+                    brewery.setBreweryType(maybeType.get());
+                }
+
+                return breweryRepository.save(brewery);
+            }
+            return null;
+        }).filter(Objects::nonNull).count() >= 1;
 
     }
 
+    private boolean reachedLastPage(List<CompletableFuture<Boolean>> requests) throws InterruptedException, ExecutionException {
+        for (CompletableFuture<Boolean> r : requests) {
+            if (!r.join()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private HttpClient getClient() {
+        return newBuilder()
+                .version(Version.HTTP_1_1)
+                .followRedirects(Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+    }
+
+    private HttpRequest getRequest(int i) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(String.format(OPENBREWERYDB_URL, i)))
+                .timeout(Duration.ofMinutes(1))
+                .header("Accept", "application/json")
+                .GET()
+                .build();
+    }
 
     @Getter
     @Setter
@@ -38,11 +154,15 @@ public class OpenBreweryDBService {
         private String street;
         private String city;
         private String state;
-        @SerializedName("postal_code") private String postalCode;
+        @SerializedName("postal_code")
+        private String postalCode;
         private String country;
         private String phone;
-        @SerializedName("website_url") private String websiteUrl;
-        @SerializedName("brewery_type") private String breweryType;
-        @SerializedName("updated_at") private String updatedAt;
+        @SerializedName("website_url")
+        private String websiteUrl;
+        @SerializedName("brewery_type")
+        private String breweryType;
+        @SerializedName("updated_at")
+        private String updatedAt;
     }
 }
