0bff723e55ae3f3a78a73e52f8e859e9100d2ae3
Rigoberto L. Perez <rlperez@udel.edu>
2020-07-19 17:28:44 -0400

    Add brew type repository

--- src/main/java/edu/rlperez/java_general_web_demo/services/BreweryService.java
+++ src/main/java/edu/rlperez/java_general_web_demo/services/BreweryService.java
@@ -1,7 +1,9 @@
 package edu.rlperez.java_general_web_demo.services;
 
 import edu.rlperez.java_general_web_demo.models.Brewery;
+import edu.rlperez.java_general_web_demo.models.BreweryType;
 import edu.rlperez.java_general_web_demo.repositories.BreweryRepository;
+import edu.rlperez.java_general_web_demo.repositories.BreweryTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,19 +11,27 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class BreweryService {
 
     private final BreweryRepository breweryRepository;
+    private final BreweryTypeRepository breweryTypeRepository;
 
     @Autowired
-    public BreweryService(BreweryRepository breweryRepository) {
+    public BreweryService(BreweryRepository breweryRepository, BreweryTypeRepository breweryTypeRepository) {
         this.breweryRepository = breweryRepository;
+        this.breweryTypeRepository = breweryTypeRepository;
     }
     
     public Page<Brewery> findAll(int pageNumber, int rowsPerPage) {
         Pageable sortedByName = PageRequest.of(pageNumber - 1, rowsPerPage, Sort.by("name").ascending());
         return breweryRepository.findAll(sortedByName);
+    }
+
+    public List<BreweryType> findAllBreweryTypes() {
+        return breweryTypeRepository.findAll();
     }
 }
